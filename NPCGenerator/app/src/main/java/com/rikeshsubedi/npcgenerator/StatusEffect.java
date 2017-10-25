e8991cc5c940583884113f397a91a4a9a29dacba
Rikesh Subedi <rsubedi3@gatech.edu>
2017-10-24 22:27:36 -0400

    Revised StatusEffect enum

---
+++ NPCGenerator/app/src/main/java/com/rikeshsubedi/npcgenerator/StatusEffect.java
@@ -0,0 +1,24 @@
+package com.rikeshsubedi.npcgenerator;
+
+/**
+ * @author Rikesh Subedi
+ * Created by keshr on 10/24/2017.
+ */
+
+public enum StatusEffect {
+    BLINDED,
+    CHARMED,
+    DEAFENED,
+    GRAPPLED,
+    FATIGUED,
+    FRIGHTENED,
+    INCAPACITATED,
+    INVISIBLE,
+    PARALYZED,
+    PETRIFIED,
+    POISONED,
+    PRONE,
+    RESTRAINED,
+    STUNNED,
+    UNCONSCIOUS
+}
